2695fcf5efa4dfe061d63008abda243734a734bb
xoventechdev <alkarim.doha@gmail.com>
2024-01-02 18:30:50 +0300

    w3schools JS

---
+++ module_05/w3schools JS/app.js
@@ -0,0 +1,20 @@
+let txtchanger  = document.getElementsByClassName("txt-changer")[0];
+
+txtchanger.addEventListener("click", function(){
+    document.getElementById("lead").innerHTML = "Is you text has changed?";
+})
+
+
+
+let bulb = document.getElementById("bulb");
+let onBulb = document.getElementById("on-btn");
+let offBulb = document.getElementById("off-btn");
+
+onBulb.addEventListener("click", function(){
+    bulb.src = "https://cdn3.vectorstock.com/i/1000x1000/43/07/light-bulb-on-black-vector-1094307.jpg";
+});
+
+offBulb.addEventListener("click", function(){
+    bulb.src = "https://previews.123rf.com/images/peshkova/peshkova1206/peshkova120600306/14060282-light-bulb-on-a-white-background.jpg";
+});
+
